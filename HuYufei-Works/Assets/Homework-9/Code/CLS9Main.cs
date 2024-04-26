22055fb1ecee31b4f30cd507eaf968edbc4dc4fe
huyufei <q8666@163.com>
2024-04-26 23:37:50 +0800

    update: 提交作业 9 的内容

---
+++ HuYufei-Works/Assets/Homework-9/Code/CLS9Main.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace Homework
+{
+    public class CLS9Main: MonoBehaviour
+    {
+
+        public Button _btnMusic1;
+        public Button _btnMusic2;
+        public Button _btnSFX1;
+        public Button _btnPlay;
+        public Button _btnStop;
+        public Button _btnPinch;
+
+
+        public AudioSource _music1;
+        public AudioSource _music2;
+        public AudioSource _sfx1;
+        public AudioMixer _mixer;
+
+        private AudioSource _currentAudio;
+
+        private void Awake()
+        {
+            
+            _btnMusic1.onClick.AddListener(OnPlayMusic1);
+            _btnMusic2.onClick.AddListener(OnPlayMusic2);
+            _btnSFX1.onClick.AddListener(OnPlaySFX1);
+            
+            _btnPlay.onClick.AddListener(OnPlaySound);
+            _btnStop.onClick.AddListener(OnStopSound);
+            _btnPinch.onClick.AddListener(OnPitchSound);
+            
+            
+            
+            
+        }
+
+
+
+        void OnPlayMusic1()
+        {
+            _currentAudio = _music1;
+            OnPlaySound();
+        }
+        
+        void OnPlayMusic2()
+        {
+            _currentAudio = _music2;
+            OnPlaySound();
+        }
+
+        void OnPlaySFX1()
+        {
+            _currentAudio = _sfx1;
+            OnPlaySound();
+        }
+
+        void OnPlaySound()
+        {
+            OnStopSound();
+            _currentAudio.Play();
+            _currentAudio.pitch = 1f;
+        }
+        
+        void OnStopSound()
+        {
+            if (_currentAudio != null) _currentAudio.Stop();
+        }
+        
+        void OnPitchSound()
+        {
+            OnStopSound();
+            _currentAudio.Play();
+            _currentAudio.pitch = 1.5f;
+        }
+
+    }
+}
